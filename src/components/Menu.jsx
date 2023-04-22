237061006f6cb155ed90516d6df4e3b22eae3e39
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2023-04-22 11:59:29 -0500

    Completando el folder components

---
+++ src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Menu.scss';
+
+const Menu = () => {
+  return (
+      <div className="desktop-menu">
+          <ul>
+              <li><a href="" className="title">My orders</a></li>
+              <li><a href="" className="title">My account</a></li>
+              <li><a href="" className="title">Sign out</a></li>
+          </ul>
+      </div>
+  );
+}
+
+export default Menu
---
+++ src/components/OrderItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/OrderItem.scss';
+
+const OrderItem = () => {
+  return (
+      <div class="OrderItem">
+          <figure>
+              <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="pc"/>
+          </figure>
+          <p>Computer</p>
+          <p>$ 100.00</p>
+          <img src="./icons/icon_close.png" alt="closed"/>
+      </div>
+  )
+}
+
+export default OrderItem
