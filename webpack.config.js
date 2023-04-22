65c24f6f28b47f2cbd20b077f01ea17948aae00a
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2023-04-21 21:40:59 -0500

    react-router-dom v6^

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     entry: './src/index.js', //punto de entrada de la aplicación
     output:{
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/'
     },
     mode: 'development',
     watch: false,
