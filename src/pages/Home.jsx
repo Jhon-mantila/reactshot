768660f3d2a310e459e960c8b8eff6350745294d
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2023-04-21 21:56:33 -0500

    Creando el header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const Home = () => {
     return (
-        <div>
+        /*<div>
             <h1>React Shop</h1>
-        </div>
+        </div>*/
+        <Header />
     );
 }
 
