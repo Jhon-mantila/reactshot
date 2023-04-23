a1b54e8ec7a98a7b297821de8a56d87af1fb454f
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2023-04-23 12:29:23 -0500

    Routes en app.jsx

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,6 +5,12 @@ import Home from '../pages/Home';
 import Login from '../pages/Login';
 import RecoveryPassword from '../pages/RecoveryPassword';
 
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
 
 import NotFound from '../pages/NotFound';
 import '../styles/global.css';
@@ -19,6 +25,12 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/recovery-password" element={<RecoveryPassword />} />
+          <Route path='/send-email' element={<SendEmail />} />
+          <Route path='/new-password' element={<NewPassword />} />
+          <Route path='/account' element={<MyAccount />} />
+          <Route path='/signup' element={<CreateAccount />} />
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/orders' element={<Orders />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
