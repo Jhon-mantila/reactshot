17b696ffa9ec958a5e2b7c68d78c88adad0cc9d8
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2023-04-26 20:35:17 -0500

    inicio consumo de api

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import ProductItem from '@components/ProductItem';
 import '@styles/ProductList.scss';
+import axios from 'axios';
+
+const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
+  
+  const [products, setProduct] = useState([]);
+
+  useEffect(() =>{
+    //const response = await axios(API);
+    //getData();
+    getDatafetch();
+    
+  },[]);
+
+  //se puede consumir la api ya sea con fetch o axios recomiendan mejor con fetch
+  const getDatafetch = async() =>{
+    const response = await fetch(API);
+    const data = await response.json();
+    console.log(data);
+    setProduct(data);
+  }
+
+  const getData = async()=>{
+    const response = await axios.get(API);
+    setProduct(response.data);
+  }
   return (
       <section className="main-container">
           <div className="cards-container">
-            <ProductItem />
+            {products.map(product =>(
+              <ProductItem />
+            ))}
           </div>
       </section>
   )
